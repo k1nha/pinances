77890946b075eaa6d1f0a9d96aa51a8bae8f979c
camp.us <lucascmpus@gmail.com>
2023-09-14 23:13:17 -0300

    feat: provider / login

---
+++ src/modules/user/form/login.form.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input";
+
+export function LoginForm() {
+  return (
+    <form>
+      <Input />
+    </form>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
-    <main className={"min-h-screen w-full flex items-center justify-center"}>
-      
-    </main>
+    <main
+      className={"min-h-screen w-full flex items-center justify-center"}></main>
   );
 }
